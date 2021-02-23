dd779c077e78b80b54b7b34d13425548eda3e451
lunarised <lunarised@outlook.com>
2021-02-24 11:33:32 +1300

    Added DTSL thing

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -12,6 +12,8 @@ Taken from About me
     <br>
     <p> I worked as a temporary Service Desk Analyst for the SDHB over the Covid-19 Lockdown to help their employees with their 
         working from home setups </p>
+
+
     <hr>
       <h3> Otago Regional Council </h3>
       <h4 class="pushLeft"><i> Service Desk Agent </i> </h4> <h4 class="pushRight">August 2020 - Current</h4>
@@ -24,14 +26,37 @@ Taken from About me
             council. Laptop setups required setting up VPN's, Connecting users to a VOIP system, Imaging the Systems via
             SCCM, as well as working with the users to get role specific apps working as intended. </p>
         <hr>
+
+      <h3 style="float: left;"> DTSL - </h3> 
+      <h4 class="break" style="padding-top=20px;"> Contracted to Otago Regional Council   </h4>
+      
+<div style="clear: both;"></div>
+      <h4 class="pushLeft"><i> User Support Engineer </i> </h4> <h4 class="pushRight">February 1 2020 - February 12 2020</h4>
+    <div style="clear: both;"></div>
+
+      <br>
+    
+        <p> After one of the onsite NTT engineers took time off, I was asked to be subcontracted from DTSL to assist NTT for their onsite 
+          service desk in the mornings during his absence. In this role, I was able to help with level 2 tickets, both diagnosing problems and finding solution to them.
+
+          This role gave me more experience in being self managed as well as letting me broaden what sort of tickets I was able to do
+           </p>
+        <hr>
     </div>
   </section> 
 </template>
 <style scoped>
 .pushLeft{
     float: left;
+
+}
+
+.break{
+  padding-top: 23px;
+  padding-left: 70px;
 }
 .pushRight{
 float: right;
+
 }
 </style>
