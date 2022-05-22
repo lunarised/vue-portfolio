dd4b108e84d245c22aab894cb95861eab75cd77d
lunarised <lunarised@outlook.com>
2022-05-22 18:08:51 +1200

    Added AD Instruments as my current workplace

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -2,61 +2,88 @@
 Taken from About me
 -->
 <template>
-  <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="experience">
+  <section
+    class="resume-section p-3 p-lg-5 d-flex align-items-center"
+    id="experience"
+  >
     <div class="w-100">
       <h2>Experience</h2>
-    <br>
-    <h3> Southern District Health Board </h3>
-     <h4 class="pushLeft"><i> Service Desk Analyst </i> </h4> <h4 class="pushRight">March 2020 - April 2020</h4>
-    <div style="clear: both;"></div> 
-    <br>
-    <p> I worked as a temporary Service Desk Analyst for the SDHB over the Covid-19 Lockdown to help their employees with their 
-        working from home setups </p>
+      <br />
+      <h3>Southern District Health Board</h3>
+      <h4 class="pushLeft"><i> Service Desk Analyst </i></h4>
+      <h4 class="pushRight">March 2020 - April 2020</h4>
+      <div style="clear: both"></div>
+      <br />
+      <p>
+        I worked as a temporary Service Desk Analyst for the SDHB over the
+        Covid-19 Lockdown to help their employees with their working from home
+        setups
+      </p>
 
+      <hr />
+      <h3>Otago Regional Council</h3>
+      <h4 class="pushLeft"><i> Service Desk Agent </i></h4>
+      <h4 class="pushRight">August 2020 - March 2021</h4>
+      <div style="clear: both"></div>
 
-    <hr>
-      <h3> Otago Regional Council </h3>
-      <h4 class="pushLeft"><i> Service Desk Agent </i> </h4> <h4 class="pushRight">August 2020 - Current</h4>
-    <div style="clear: both;"></div>
+      <br />
 
-      <br>
-    
-        <p> My core responsibilities as my role as Service Desk Lead, were to provide level 1 support for tickets
-            that came through our ticketing system, as well as to document, setup and deploy equiptment across the 
-            council. Laptop setups required setting up VPN's, Connecting users to a VOIP system, Imaging the Systems via
-            SCCM, as well as working with the users to get role specific apps working as intended. </p>
-        <hr>
+      <p>
+        My core responsibilities as my role as Service Desk Lead, were to
+        provide level 1 support for tickets that came through our ticketing
+        system, as well as to document, setup and deploy equiptment across the
+        council. Laptop setups required setting up VPN's, Connecting users to a
+        VOIP system, Imaging the Systems via SCCM, as well as working with the
+        users to get role specific apps working as intended.
+      </p>
+      <hr />
 
-      <h3 style="float: left;"> DTSL - </h3> 
-      <h4 class="break" style="padding-top=20px;"> Contracted to Otago Regional Council   </h4>
-      
-<div style="clear: both;"></div>
-      <h4 class="pushLeft"><i> User Support Engineer </i> </h4> <h4 class="pushRight">February 1 2020 - February 12 2020</h4>
-    <div style="clear: both;"></div>
+      <h3 style="float: left">DTSL -</h3>
+      <h4 class="break" style="padding-top=20px;">
+        Contracted to Otago Regional Council
+      </h4>
 
-      <br>
-    
-        <p> After one of the onsite NTT engineers took time off, I was asked to be subcontracted from DTSL to assist NTT for their onsite 
-          service desk in the mornings during his absence. In this role, I was able to help with level 2 tickets, both diagnosing problems and finding solution to them.
+      <div style="clear: both"></div>
+      <h4 class="pushLeft"><i> User Support Engineer </i></h4>
+      <h4 class="pushRight">February 1 2021 - February 12 2021</h4>
+      <div style="clear: both"></div>
 
-          This role gave me more experience in being self managed as well as letting me broaden what sort of tickets I was able to do
-           </p>
-        <hr>
+      <br />
+
+      <p>
+        After one of the onsite NTT engineers took time off, I was asked to be
+        subcontracted from DTSL to assist NTT for their onsite service desk in
+        the mornings during his absence. In this role, I was able to help with
+        level 2 tickets, both diagnosing problems and finding solution to them.
+        This role gave me more experience in being self managed as well as
+        letting me broaden what sort of tickets I was able to do
+      </p>
+      <hr />
+      <h3>AD Instruments</h3>
+      <h4 class="pushLeft"><i> Junior Software Engineer </i></h4>
+      <h4 class="pushRight">April 2021 - Current</h4>
+      <div style="clear: both"></div>
+      <br />
+      <p>
+        My Core responsibilities in my role at ADI is to write high quality Typescript, 
+        React, CSS and occasionally C++ code to help further development in LabChart Lightning
+      </p>
+
+      <hr />
+
     </div>
-  </section> 
+  </section>
 </template>
 <style scoped>
-.pushLeft{
-    float: left;
-
+.pushLeft {
+  float: left;
 }
 
-.break{
+.break {
   padding-top: 23px;
   padding-left: 70px;
 }
-.pushRight{
-float: right;
-
+.pushRight {
+  float: right;
 }
 </style>
